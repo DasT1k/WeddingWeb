b126c45c641887919a5b8a97062ee1f1be5785ef
Roman Bokhovko <romanbokhovko@gmail.com>
2024-04-06 09:34:41 +0300

    updated requirements to create venv in pythonanywhere, adopted styles to mobile view, added proper hints to food options

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+# this file better be named something like "flask_app.py" to be used with pythonanywhere
+# as when "app.py" it can lead to conflict in WSGI config with "from app import app as application"
+
 from app import create_app
 
 app = create_app()
